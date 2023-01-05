fa346e126b8d3b09a626dab7c2f798f41115644b
Francis Tse <wtse.ucsd@gmail.com>
2023-01-05 11:18:40 -0800

    fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,17 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function seed() {
-  await Promise.all(getJobs().map((job) => db.jobs.create({ data: job })));
+  await Promise.all(getJobs().map((job) => db.job.create({ data: job })));
 }
 
 seed();
 
-const user = {
-  id: "1",
-  email: "wtse.ucsd@gmail.com",
-  name: "Francis",
-  jobs: [],
-};
+// const user = {
+//   id: "1",
+//   email: "wtse.ucsd@gmail.com",
+//   name: "Francis",
+//   jobs: [],
+// };
 
 function getJobs() {
   return [
@@ -22,8 +22,8 @@ function getJobs() {
       company: "X",
       title: "Software Engineer",
       description: "A job at X company",
-      user,
-      userId: user.id,
+      // user,
+      // userId: user.id,
       createdAt: new Date("2022-12-28"),
     },
     {
@@ -31,8 +31,8 @@ function getJobs() {
       title: "Software Engineer 2",
       company: "Y",
       description: "A job at Y company",
-      user,
-      userId: user.id,
+      // user,
+      // userId: user.id,
       createdAt: new Date("2022-12-30"),
     },
     {
@@ -40,8 +40,8 @@ function getJobs() {
       company: "Z",
       title: "Senior Software Engineer",
       description: "A job at Z company",
-      user,
-      userId: user.id,
+      // user,
+      // userId: user.id,
       createdAt: new Date("2023-01-02"),
     },
   ];
