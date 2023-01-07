877252e8c964c4fd294d930d58a35e572437acee
Francis Tse <wtse.ucsd@gmail.com>
2023-01-06 21:08:12 -0800

    add selector

---
+++ app/selectors/selectJobsByColumn.ts
@@ -0,0 +1,20 @@
+import type { Job } from "~/types";
+
+export function selectJobsByColumn(jobs: Job[]) {
+  const jobsByColumn: Record<string, Job[]> = {};
+
+  for (const job of jobs) {
+    // defaults to 'applied' if there's no status associated with the job card
+    if (!job.status) {
+      jobsByColumn["applied"] = [...jobsByColumn["applied"], job];
+    } else {
+      if (job.status in jobsByColumn) {
+        jobsByColumn[job.status] = [...jobsByColumn[job.status], job];
+      } else {
+        jobsByColumn[job.status] = [job];
+      }
+    }
+  }
+
+  return jobsByColumn;
+}
