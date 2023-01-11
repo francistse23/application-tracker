c485644bd82dad3bec075b7a9ea0ba01c58293bb
Francis Tse <wtse.ucsd@gmail.com>
2023-01-10 22:18:58 -0800

    add modal state to boards

--- app/routes/boards.tsx
+++ app/routes/boards.tsx
@@ -1,6 +1,9 @@
+import * as React from "react";
+
 import { Outlet, useLoaderData } from "@remix-run/react";
 
 import Column from "~/components/Column";
+import CreateJobModal from "~/components/CreateJobModal";
 import type { Job } from "~/types";
 import { db } from "~/utils/db.server";
 import { json } from "@remix-run/node";
@@ -15,15 +18,20 @@ export const loader = async () => {
 
 export default function Boards() {
   const { jobs } = useLoaderData<typeof loader>();
+  const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
 
   return (
     <div>
       <Outlet />
       <div>
+        {isModalOpen && <CreateJobModal />}
         {Object.entries(jobs).map(([columnName, jobsInColumn]) => (
           <Column
             key={columnName}
             name={columnName}
+            setIsModalOpen={setIsModalOpen}
+            // manually casting because of Remix's typing issue
+            // jobs is being typed as SerializeObject<UnknownToOptional<T>>
             jobs={jobsInColumn as unknown as Job[]}
           />
         ))}
