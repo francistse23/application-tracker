9ea0c172125655691895116985d20a3621d0d3d0
Francis Tse <wtse.ucsd@gmail.com>
2023-01-10 22:21:45 -0800

    add typing to handle modal opening in Column

--- app/components/Column.tsx
+++ app/components/Column.tsx
@@ -1,11 +1,29 @@
+import * as React from "react";
+
+import type { Dispatch, SetStateAction } from "react";
+
 import Card from "~/components/Card";
 import type { Job } from "~/types";
 
-export default function Column({ name, jobs }: { name: string; jobs: Job[] }) {
+type ColumnProps = {
+  name: string;
+  jobs: Job[];
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function Column({ name, jobs, setIsModalOpen }: ColumnProps) {
+  const handleModalOpen = React.useCallback(
+    () => setIsModalOpen((state: boolean) => !state),
+    [setIsModalOpen]
+  );
+
   return (
     <div>
       <h3>{name}</h3>
       <h4>{jobs.length} jobs</h4>
+
+      <button onClick={handleModalOpen}>+</button>
+
       <div>
         {jobs.map((job) => (
           <Card key={job.id} job={job} />
