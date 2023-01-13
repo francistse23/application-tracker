d98aaf116f131914fad0a9fb266aa2f30b37828f
Francis Tse <wtse.ucsd@gmail.com>
2023-01-12 23:16:59 -0800

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,58 +1,120 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
 async function seed() {
-  await Promise.all(getJobs().map((job) => db.job.create({ data: job })));
-}
+  const user = await db.user.upsert({
+    where: { email: "wtse.ucsd@gmail.com" },
+    update: {},
+    create: {
+      email: "wtse.ucsd@gmail.com",
+      name: "Francis",
+      boards: {
+        create: [
+          {
+            name: "default",
+          },
+        ],
+      },
+    },
+  });
 
-seed();
+  const defaultBoard = await db.board.findUniqueOrThrow({
+    where: {
+      name: "default",
+    },
+  });
 
-// const user = {
-//   id: "1",
-//   email: "wtse.ucsd@gmail.com",
-//   name: "Francis",
-//   jobs: [],
-// };
-
-function getJobs() {
-  return [
+  const lists: Prisma.ListCreateInput[] = [
     {
-      id: "1",
+      name: "applied",
+      sortOrder: 1,
+      board: {
+        connect: {
+          id: defaultBoard.id,
+        },
+      },
+    },
+    {
+      name: "interviewing",
+      sortOrder: 2,
+      board: {
+        connect: {
+          id: defaultBoard.id,
+        },
+      },
+    },
+  ];
+
+  const [applied, interviewing] = await Promise.all(
+    lists.map(async (list) => await db.list.create({ data: list }))
+  );
+
+  const jobs: Prisma.JobCreateInput[] = [
+    {
       company: "X",
       title: "Software Engineer",
-      description: "A job at X company",
-      // user,
-      // userId: user.id,
-      createdAt: new Date("2022-12-28"),
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+      list: {
+        connect: {
+          id: applied.id,
+        },
+      },
     },
+    // {
+    //   title: "Software Engineer 2",
+    //   company: "Y",
+    //   description: "A job at Y company",
+    //   user: {
+    //     connect: {
+    //       id: user.id,
+    //     },
+    //   },
+    //   list: {
+    //     connect: {
+    //       id: applied.id,
+    //     },
+    //   },
+    // },
+    // {
+    //   company: "Z",
+    //   title: "Senior Software Engineer",
+    //   description: "A job at Z company",
+    //   user: {
+    //     connect: {
+    //       id: user.id,
+    //     },
+    //   },
+    //   list: {
+    //     connect: {
+    //       id: applied.id,
+    //     },
+    //   },
+    // },
     {
-      id: "2",
-      title: "Software Engineer 2",
-      company: "Y",
-      description: "A job at Y company",
-      // user,
-      // userId: user.id,
-      createdAt: new Date("2022-12-30"),
-    },
-    {
-      id: "3",
-      company: "Z",
-      title: "Senior Software Engineer",
-      description: "A job at Z company",
-      // user,
-      // userId: user.id,
-      createdAt: new Date("2023-01-02"),
-    },
-    {
-      id: "4",
       company: "XZ",
       title: "Mid level Software Engineer",
       description: "A job at XZ company",
-      // user,
-      // userId: user.id,
-      createdAt: new Date("2023-01-02"),
-      status: "interviewing",
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+      list: {
+        connect: {
+          id: interviewing.id,
+        },
+      },
     },
   ];
+
+  const defaultJobs = await Promise.all(
+    jobs.map((job) => db.job.create({ data: job }))
+  );
 }
+
+seed();
