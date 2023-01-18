5d87d3cf7342bee0e187ffe5e3ea4782f104fbf0
Francis Tse <wtse.ucsd@gmail.com>
2023-01-17 21:18:33 -0800

    add helper functions to validate input

---
+++ app/utils/helpers.ts
@@ -0,0 +1,20 @@
+export function validateUsername(username: unknown) {
+  if (typeof username !== "string" || username.length < 3) {
+    return `Usernames must be at least 3 characters long`;
+  }
+}
+
+export function validatePassword(password: unknown) {
+  if (typeof password !== "string" || password.length < 6) {
+    return `Passwords must be at least 6 characters long`;
+  }
+}
+
+export function validateUrl(url: string) {
+  const urls = ["/boards", "/"];
+
+  if (urls.includes(url)) {
+    return url;
+  }
+  return "/";
+}
