9de9491916d7ffa409cff9be2bb5aee3f25303f0
Francis Tse <wtse.ucsd@gmail.com>
2023-01-18 20:16:53 -0800

    add helper functions to login and create user session

---
+++ app/utils/session.server.ts
@@ -0,0 +1,58 @@
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
+
+import bcrypt from "bcryptjs";
+import { db } from "./db.server";
+
+type LoginForm = {
+  username: string;
+  password: string;
+};
+
+export const login = async ({ username, password }: LoginForm) => {
+  const user = await db.user.findUnique({
+    where: { username },
+  });
+
+  if (!user) return null;
+
+  const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
+
+  if (!isCorrectPassword) return null;
+
+  return { id: user.id, username };
+};
+
+const sessionSecret = process.env.SESSION_SECRET;
+
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "Land_It_session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+type UserSession = {
+  userId: string;
+  redirectTo: string;
+};
+
+export const createUserSession = async ({
+  userId,
+  redirectTo,
+}: UserSession) => {
+  const session = await storage.getSession();
+  session.set("userId", userId);
+
+  return redirect(redirectTo, {
+    headers: { "Set-Cookie": await storage.commitSession(session) },
+  });
+};
