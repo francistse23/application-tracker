3ea259ccaebbfad04f9c4601bc40375adcb3e7ba
Francis Tse <wtse.ucsd@gmail.com>
2023-01-18 21:20:55 -0800

    add more helper functions to handle getting user session and user id

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -56,3 +56,32 @@ export const createUserSession = async ({
     headers: { "Set-Cookie": await storage.commitSession(session) },
   });
 };
+
+function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+
+  if (!userId || typeof userId !== "string") return null;
+
+  return userId;
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+
+    throw redirect(`login?${searchParams}`);
+  }
+
+  return userId;
+}
