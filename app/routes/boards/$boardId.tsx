203997aeee9d4010ead96026edb5d5bdfa94779f
Francis Tse <wtse.ucsd@gmail.com>
2023-01-19 20:21:04 -0800

    add user login status rendering
    handle add job modal

--- app/routes/boards/$boardId.tsx
+++ app/routes/boards/$boardId.tsx
@@ -6,23 +6,41 @@ import { db } from "~/utils/db.server";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-export const loader = async () => {
-  const board = await db.board.findUniqueOrThrow({
-    where: { name: "default" },
-    select: { lists: { include: { jobs: true } } },
+export const loader = async ({ params, request }: LoaderArgs) => {
+  const user = await getUser(request);
+  const lists = await db.list.findMany({
+    where: { boardId: Number(params.boardId) },
+    include: { jobs: true },
   });
 
-  return json({ lists: board.lists });
+  return json({ lists, user });
 };
 
 export default function BoardRoute() {
-  const { lists } = useLoaderData<typeof loader>();
+  const { lists, user } = useLoaderData<typeof loader>();
   const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
   const [selectedListId, setSelectedListId] = React.useState<number>(-1);
 
   return (
     <div>
-      {isModalOpen && <CreateJobModal listId={selectedListId} />}
+      {user ? (
+        <div>
+          <span>{`Hi ${user.username}`}</span>
+          <form action="/logout" method="post">
+            <button type="submit">Logout</button>
+          </form>
+        </div>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+
+      {isModalOpen && (
+        <CreateJobModal
+          listId={selectedListId}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
+
       {lists
         .sort((a, b) => a.sortOrder - b.sortOrder)
         .map(({ id, name, jobs }) => (
