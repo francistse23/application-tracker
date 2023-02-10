1347317929faefb093915a213d0377068e7a4769
Francis Tse <wtse.ucsd@gmail.com>
2023-02-09 18:06:40 -0800

    add style for index route and link stylesheets

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,20 @@
 import { Link } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Land It</h1>
-      <h2>Your one stop job board</h2>
-      <Link to="boards/1">Go to your board</Link>
-      <Link to="login">Login</Link>
+    <div className="container">
+      <div className="content">
+        <h1>Welcome to Land It</h1>
+        <h2>Your one stop job board</h2>
+        <Link to="boards/1">Go to your board</Link>
+        <Link to="login">Login</Link>
+      </div>
     </div>
   );
 }
