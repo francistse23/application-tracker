8121b91cf6c7f020f6b496671b47332dc5385b5c
Francis Tse <wtse.ucsd@gmail.com>
2023-02-16 20:44:11 -0800

    add style to board

--- app/routes/boards/$boardId.tsx
+++ app/routes/boards/$boardId.tsx
@@ -2,13 +2,27 @@ import Column from "~/components/Column";
 import type { Job } from "~/types";
 import React from "react";
 import { db } from "~/utils/db.server";
-import { json, type LoaderArgs, type ActionArgs } from "@remix-run/node";
-import { Link, useCatch, useLoaderData, useParams } from "@remix-run/react";
+import {
+  json,
+  redirect,
+  type LoaderArgs,
+  type ActionArgs,
+  type LinksFunction,
+} from "@remix-run/node";
+import {
+  Form,
+  Link,
+  useCatch,
+  useLoaderData,
+  useParams,
+} from "@remix-run/react";
 import { z } from "zod";
 import { getUser, requireUserId } from "~/utils/session.server";
 import AddJobModal from "~/components/AddJobModal";
 
 import { CardDetailModal } from "~/components/CardDetailModal";
+
+import stylesUrl from "~/styles/board.css";
 
 const FormSchema = z.object({
   company: z.string().min(1),
@@ -47,42 +61,48 @@ export const action = async ({ request }: ActionArgs) => {
       return null;
     }
     default: {
-    const company = form.get("company");
-    const title = form.get("jobTitle");
-    const listId = Number(form.get("list"));
+      const company = form.get("company");
+      const title = form.get("jobTitle");
+      const listId = Number(form.get("list"));
 
-    const data = {
-      company,
-      title,
-      listId,
-      userId,
-    };
+      const data = {
+        company,
+        title,
+        listId,
+        userId,
+      };
 
-    const result = FormSchema.safeParse(data);
+      const result = FormSchema.safeParse(data);
 
-    if (!result.success) {
-      return json({ error: result.error.issues });
-    } else {
-      const res = await db.job.create({
-        data: result.data,
-      });
+      if (!result.success) {
+        return json({ error: result.error.issues });
+      } else {
+        const res = await db.job.create({
+          data: result.data,
+        });
 
-      return res;
+        return res;
+      }
     }
-    }
-    }
+  }
 };
 
-    await db.job.delete({ where: { id: job.id } });
-    return null;
-  }
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
 export const loader = async ({ params, request }: LoaderArgs) => {
   const user = await getUser(request);
+
+  if (!user) {
+    throw redirect("/login");
+  }
+
   const board = await db.board.findUnique({
     where: { id: Number(params.boardId) },
+    select: { name: true },
   });
+
   const lists = await db.list.findMany({
     where: { boardId: Number(params.boardId) },
     include: { jobs: true },
@@ -92,11 +112,11 @@ export const loader = async ({ params, request }: LoaderArgs) => {
     throw new Response("Uh oh. We cannot find the job board", { status: 404 });
   }
 
-  return json({ lists, user });
+  return json({ lists, user, board });
 };
 
 export default function BoardRoute() {
-  const { lists, user } = useLoaderData<typeof loader>();
+  const { lists, user, board } = useLoaderData<typeof loader>();
   // add job modal states
   const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
   const [selectedListId, setSelectedListId] = React.useState<number>(-1);
@@ -110,40 +130,55 @@ export default function BoardRoute() {
   }, []);
 
   return (
-    <div>
+    <div className={`board-container`}>
       {user ? (
-        <div>
-          <span>{`Hi ${user.username}`}</span>
-          <form action="/logout" method="post">
-            <button type="submit">Logout</button>
-          </form>
+        <div className="navbar">
+          <h4>{`Hi, ${user.username}`}</h4>
+          {/* TODO: add edit board name function */}
+          <h4>{board.name}</h4>
+          <Form action="/logout" method="post">
+            <button className="button" type="submit">
+              Logout
+            </button>
+          </Form>
         </div>
       ) : (
         <Link to="/login">Login</Link>
       )}
+
       {isModalOpen && (
         <AddJobModal listId={selectedListId} setIsModalOpen={setIsModalOpen} />
       )}
 
-      {/* // confirm modal to delete? */}
       {isDetailOpen && (
-        <CardDetailModal job={currentJob} setIsDetailOpen={setIsDetailOpen} />
+        <CardDetailModal
+          job={currentJob}
+          setIsDetailOpen={setIsDetailOpen}
+          lists={lists}
+        />
       )}
-      {lists
-        .sort((a, b) => a.sortOrder - b.sortOrder)
-        .map(({ id, name, jobs }) => (
-          <Column
-            key={id}
-            listId={id}
-            name={name}
-            handleClick={handleOnCardClick}
-            setIsModalOpen={setIsModalOpen}
-            setSelectedListId={setSelectedListId}
-            // manually casting because of Remix's typing issue
-            // jobs is being typed as SerializeObject<UnknownToOptional<T>>/>
-            jobs={jobs as unknown as Job[]}
-          />
-        ))}
+
+      {user ? (
+        <div className="lists-container">
+          {lists
+            .sort((a, b) => a.sortOrder - b.sortOrder)
+            .map(({ id, name, jobs }) => (
+              <Column
+                key={id}
+                listId={id}
+                name={name}
+                handleClick={handleOnCardClick}
+                setIsModalOpen={setIsModalOpen}
+                setSelectedListId={setSelectedListId}
+                // manually casting because of Remix's typing issue
+                // jobs is being typed as SerializeObject<UnknownToOptional<T>>/>
+                jobs={jobs as unknown as Job[]}
+              />
+            ))}
+        </div>
+      ) : (
+        <div>Looks like you're not logged in!</div>
+      )}
     </div>
   );
 }
