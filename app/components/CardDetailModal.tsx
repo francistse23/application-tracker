c762272c2b3cf2453b490f77d4fb27a32d22cb44
Francis Tse <wtse.ucsd@gmail.com>
2023-02-16 20:37:28 -0800

    add style for job detail modal

--- app/components/CardDetailModal.tsx
+++ app/components/CardDetailModal.tsx
@@ -1,62 +1,86 @@
 import * as React from "react";
 
 import type { Dispatch, SetStateAction } from "react";
+import type { Job, List } from "~/types";
 
 import { Form } from "@remix-run/react";
-import type { Job } from "~/types";
 
 type CardDetailModalProps = {
   job: Job | null;
+  lists: List[];
   setIsDetailOpen: Dispatch<SetStateAction<boolean>>;
 };
 
 export const CardDetailModal = ({
   job,
   setIsDetailOpen,
+  lists,
 }: CardDetailModalProps) => {
   const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] =
     React.useState<boolean>(false);
 
   if (!job) return null;
 
-  const { company, title, description, url } = job;
+  const { company, title, description, url, listId } = job;
 
   return (
     <div className="modal-container" onClick={() => setIsDetailOpen(false)}>
       <div className="job-details" onClick={(e) => e.stopPropagation()}>
-        <button className="close-button" onClick={() => setIsDetailOpen(false)}>
-          X
-        </button>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <button
+            className="close-button"
+            onClick={() => setIsDetailOpen(false)}
+          >
+            X
+          </button>
+        </div>
+        {/* TODO: add useSubmit hook to save form on field change */}
         <Form method="post">
-          <label htmlFor="company">Company</label>
-          <input
-            id="company"
-            placeholder="Company"
-            type="text"
-            value={company}
-            required
-          />
-          <label htmlFor="title">Title</label>
-          <input
-            id="title"
-            placeholder="Job title"
-            type="text"
-            value={title}
-            required
-          />
+          <div className="form-row">
+            <div className="form-col">
+              <label htmlFor="company">Company</label>
+              <input
+                id="company"
+                placeholder="Company"
+                type="text"
+                value={company}
+                required
+              />
+              <label htmlFor="title">Title</label>
+              <input
+                id="title"
+                placeholder="Job title"
+                type="text"
+                value={title}
+                required
+              />
+            </div>
+            <div className="form-col">
+              <label htmlFor="status">Status</label>
+              <select defaultValue={listId}>
+                {lists.map(({ id, name }) => (
+                  <option key={id} value={id}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+              <label htmlFor="url">URL</label>
+              <input
+                id="url"
+                placeholder="Link to job description/company"
+                type="url"
+                defaultValue={url ?? ""}
+              />
+            </div>
+          </div>
+
           <label htmlFor="description">Job Description</label>
           <textarea
             id="description"
             placeholder="Job description"
-            value={description}
+            defaultValue={description ?? ""}
           />
-          <label htmlFor="url">URL</label>
-          <input
-            id="url"
-            placeholder="Link to job description/company"
-            type="url"
-            value={url ?? ""}
-          />
+
           <div className="buttons-container">
             <input type="hidden" name="json" value={JSON.stringify(job)} />
             <button type="submit" className="button">
@@ -71,14 +95,7 @@ export const CardDetailModal = ({
           </div>
           {isConfirmDeleteOpen && (
             <div className="modal-container">
-              <div
-                style={{
-                  backgroundColor: "var(--color-background)",
-                  borderRadius: "0.25rem",
-                  zIndex: 100,
-                  padding: "3rem 4rem",
-                }}
-              >
+              <div className="warning-modal">
                 <span>Are you sure you want to delete this job?</span>
                 <div className="buttons-container">
                   <button
